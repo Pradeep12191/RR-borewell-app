5a10df67a067a88f5f51dfe2ebbf209aacd00c46
pradeep <pradeep12191@gmail.com>
2019-10-19 04:11:31 -0700

    vehclec exchange interface added

--- src/app/models/RpmEntry.ts
+++ src/app/models/RpmEntry.ts
@@ -1,16 +1,18 @@
+import { RpmVehicleExchage } from './RpmVehicleExchange';
+
 export interface RpmEntry {
-    balance_stock_feet: number,
-    damage_feet: number,
-    mm_income: number,
-    pipe_id: number,
-    pipe_size: number,
-    pipe_type: string,
-    point_expenses_feet: number,
-    previous_stock_feet: number,
-    rr_income: number,
-    vehicle_ex_in: number,
-    vehicle_ex_out: number,
-    rr_income_feet?: number,
-    mm_income_feet?: number,
-    available_stock_feet?: number
+    balance_stock_feet: number;
+    damage_feet: number;
+    mm_income: number;
+    pipe_id: number;
+    pipe_size: number;
+    pipe_type: string;
+    point_expenses_feet: number;
+    previous_stock_feet: number;
+    rr_income: number;
+    vehicle_ex_in: number;
+    vehicle_ex_out: number;
+    rr_income_feet?: number;
+    mm_income_feet?: number;
+    available_stock_feet?: number;
 }
---
+++ src/app/models/RpmVehicleExchange.ts
@@ -0,0 +1,6 @@
+export interface RpmVehicleExchage {
+    vehicle_in_id: number;
+    vehicle_in_rpm_sheet_no: number;
+    vehicle_out_id: number;
+    vehicle_out_rpm_sheet_no: number;
+}

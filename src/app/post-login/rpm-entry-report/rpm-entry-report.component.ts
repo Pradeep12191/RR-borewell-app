6108524886ca48d6bedd51a46b3127f6772ff77f
pradeep <pradeep12191@gmail.com>
2019-11-08 22:40:11 -0800

    filter for rpm sheet report

--- src/app/post-login/rpm-entry-report/rpm-entry-report.component.ts
+++ src/app/post-login/rpm-entry-report/rpm-entry-report.component.ts
@@ -3,12 +3,37 @@ import { Subscription } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
 import { RpmEntrySheet } from '../../models/RpmEntrySheet';
 import { Column } from '../../expand-table/Column';
-import { MatTableDataSource } from '@angular/material';
+import { MatTableDataSource, MatDatepicker, } from '@angular/material';
 import { RpmEntryService } from '../rpm-entry/rpm-entry.service';
 import { RpmEntryReportService } from './rpm-entry-report.service';
 import { LoaderService } from '../../services/loader-service';
 import { finalize } from 'rxjs/operators';
+import { FormGroup, FormBuilder, AbstractControl } from '@angular/forms';
+import { Moment } from 'moment';
+import * as moment from 'moment';
 
+const sheetNoValidation = (control: AbstractControl) => {
+    const fromSheetNo = +control.get('fromRpmSheetNo').value;
+    const toSheetNo = +control.get('toRpmSheetNo').value;
+    if (fromSheetNo && toSheetNo && fromSheetNo > toSheetNo) {
+        return {
+            sheetNoGreater: true
+        }
+    }
+    return null
+}
+
+const dateValidation = (control: AbstractControl) => {
+    const fromDate = control.get('from').value as Moment;
+    const toDate = control.get('to').value as Moment;
+    console.log(fromDate, toDate)
+    if (fromDate && toDate && fromDate.isAfter(toDate)) {
+        return {
+            dateGreater: true
+        }
+    }
+    return null
+}
 
 @Component({
     templateUrl: './rpm-entry-report.component.html',
@@ -20,6 +45,8 @@ export class RpmEntryReportComponent implements OnDestroy {
     entries: RpmEntrySheet[];
     entriesDatasource: MatTableDataSource<RpmEntrySheet>;
     vehicleId;
+    filterForm: FormGroup;
+    monthPicker: MatDatepicker<any>;
     public columns: Column[] = [
         { id: 'serialNo', name: 'COLUMN.SERIAL_NO', type: 'index', width: '10' },
         { id: 'rpm_sheet_no', name: 'RPM Sheet No.', type: 'string', width: '15', isCenter: true, style: { fontSize: '20px', fontWeight: 'bold' } },
@@ -30,7 +57,8 @@ export class RpmEntryReportComponent implements OnDestroy {
     constructor(
         private route: ActivatedRoute,
         private rpmEntryReportService: RpmEntryReportService,
-        private loader: LoaderService
+        private loader: LoaderService,
+        private fb: FormBuilder
     ) {
         this.routeDataSubcription = this.route.data.subscribe((data) => {
             this.entries = data.entries;
@@ -38,6 +66,18 @@ export class RpmEntryReportComponent implements OnDestroy {
         });
         this.routeParamSubcription = this.route.queryParamMap.subscribe((params) => {
             this.vehicleId = params.get('vehicleId')
+        })
+
+        this.filterForm = this.fb.group({
+            rpm: this.fb.group({
+                fromRpmSheetNo: '',
+                toRpmSheetNo: ''
+            }, { validators: sheetNoValidation }),
+            date: this.fb.group({
+                from: '',
+                to: ''
+            }, { validators: dateValidation }),
+            month: ''
         })
     }
 
@@ -50,6 +90,14 @@ export class RpmEntryReportComponent implements OnDestroy {
         ).subscribe()
     }
 
+    getMessage() {
+        if (this.filterForm.get('date').invalid) {
+            return 'To Date should be greater than From Date'
+        }
+        if (this.filterForm.get('rpm').invalid) {
+            return 'To Rpm Sheet no should be greater than From Rpm Sheet no'
+        }
+    }
 
 
     ngOnDestroy() {
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from '../material.module';
+import { MonthPickerComponent } from './month-picker/month-picker.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
-    exports: [TranslateModule]
+    declarations: [
+        MonthPickerComponent
+    ],
+    imports: [
+        ReactiveFormsModule,
+        MaterialModule,
+        FlexLayoutModule
+    ],
+    exports: [
+        TranslateModule,
+        MonthPickerComponent
+    ]
 })
 export class SharedModule {
 

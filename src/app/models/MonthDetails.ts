88e128638b1d9c37dbe53df87004c8332cab9311
Pradeep12191 <pradeep12191@gmail.com>
2019-12-14 00:32:07 +0530

    air, hose cleaning, rebore rpm as other rpm

--- src/app/models/MonthDetails.ts
+++ src/app/models/MonthDetails.ts
@@ -4,6 +4,7 @@ export interface MonthDetails {
     m_diesel?: number;
     m_extra_feet?: number;
     m_rebore_feet?: number;
+    m_other_rpm?: number;
     m_extra_hour?: number;
     m_extra_min?: number;
     m_rpm?: number;

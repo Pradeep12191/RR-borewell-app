b543c8a039b008c06596ff9497f927d4ec3236dc
Pradeep12191 <pradeep12191@gmail.com>
2020-02-02 15:21:00 +0530

    asd

--- src/app/post-login/rpm-entry/rpm-entry-component.ts
+++ src/app/post-login/rpm-entry/rpm-entry-component.ts
@@ -2004,7 +2004,7 @@ export class RpmEntryComponent implements OnInit, OnDestroy, AfterViewInit {
                 {
                     ...vExOut,
                     rpmNo: +vExOut.rpmNo,
-                    vehicle: vExOut || { vehicle_id: 0, regNo: '', type: '' },
+                    vehicle: vExOut.vehilce || { vehicle_id: 0, regNo: '', type: '' },
                     pipes: vExOut.pipes.map(p => ({ id: +p.pipeId, type: p.pipeType, feet: +p.value || 0, size: +p.pipeSize }))
                 }
             ))
